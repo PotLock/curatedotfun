70d5584e5f47264abb867c01631c7ba8a3889588
Zeeshan Ahmad <itexpert120@outlook.com>
2025-03-26 14:58:15 +0500

    Move tabs to it's own component

---
+++ frontend/src/components/profile/ProfileTabs.tsx
@@ -0,0 +1,57 @@
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../components/ui/tabs";
+import {
+  Activity,
+  Award,
+  Newspaper,
+  NotepadTextDashed,
+  ScanSearch,
+} from "lucide-react";
+
+export function ProfileTabs() {
+  return (
+    <Tabs defaultValue="overview" className="w-full">
+      <TabsList>
+        <TabsTrigger value="overview">
+          <ScanSearch strokeWidth={1} size={24} />
+          Overview
+        </TabsTrigger>
+        <TabsTrigger value="content">
+          <NotepadTextDashed strokeWidth={1} size={24} />
+          Content
+        </TabsTrigger>
+        <TabsTrigger value="my-feeds">
+          <Newspaper strokeWidth={1} size={24} />
+          My Feeds
+        </TabsTrigger>
+        <TabsTrigger value="points">
+          <Award strokeWidth={1} size={24} />
+          Points
+        </TabsTrigger>
+        <TabsTrigger value="activity">
+          <Activity strokeWidth={1} size={24} />
+          Activity
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="overview">
+        <p>Overview</p>
+      </TabsContent>
+      <TabsContent value="content">
+        <p>Content</p>
+      </TabsContent>
+      <TabsContent value="my-feeds">
+        <p>My Feeds</p>
+      </TabsContent>
+      <TabsContent value="points">
+        <p>Points</p>
+      </TabsContent>
+      <TabsContent value="activity">
+        <p>Activity</p>
+      </TabsContent>
+    </Tabs>
+  );
+}
