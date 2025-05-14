1473e34173908a975bb13e1ef0fa957a2ee6fd36
Elliot Braem <elliot@ejlbraem.com>
2025-05-14 11:35:29 -0500

    don't distribute on staging

--- backend/src/services/config/config.service.ts
+++ backend/src/services/config/config.service.ts
@@ -11,6 +11,7 @@ import { logger } from "../../utils/logger";
 
 export const isProduction = process.env.NODE_ENV === "production";
 export const isTest = process.env.NODE_ENV === "test";
+export const isStaging = process.env.NODE_ENV === "staging";
 export class ConfigService {
   private static instance: ConfigService;
   private config: AppConfig | null = null;
