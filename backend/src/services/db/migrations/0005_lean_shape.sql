8bc9534031b4fe4bd01e07b89b1b2f37f2a31c1b
Elliot Braem <elliot@ejlbraem.com>
2025-05-30 19:32:30 -0500

    without time zone

--- backend/src/services/db/migrations/0005_lean_shape.sql
+++ backend/src/services/db/migrations/0005_lean_shape.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "submissions" ALTER COLUMN "submitted_at" SET DATA TYPE timestamp;
+ALTER TABLE "submissions" ALTER COLUMN "submitted_at" SET DATA TYPE timestamp USING "submitted_at"::timestamp without time zone;
