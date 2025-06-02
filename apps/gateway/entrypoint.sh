c03a909341cc14df1b94a020ab362425b17a8ae3
Elliot Braem <elliot@ejlbraem.com>
2025-06-02 16:38:35 -0500

    matching host

--- apps/gateway/entrypoint.sh
+++ apps/gateway/entrypoint.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 export APP_HOST=${APP_DOMAIN##*://}
 
 echo using API: ${API_DOMAIN} with port: ${API_PORT}
-echo using APP: ${APP_DOMAIN} with host: ${APP_HOST}
+echo using APP: ${APP_DOMAIN}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
