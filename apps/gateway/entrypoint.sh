51440bc391445b34984ab57efdfc544b77f8f4a7
Elliot Braem <elliot@ejlbraem.com>
2025-06-02 16:26:22 -0500

    set domain adn host

--- apps/gateway/entrypoint.sh
+++ apps/gateway/entrypoint.sh
@@ -2,18 +2,10 @@
 
 set -euo pipefail
 
-# for backwards compatibility, seperates host and port from url
-# export FRONTEND_DOMAIN=${FRONTEND_DOMAIN:-${FRONTEND_HOST%:*}}
-# export FRONTEND_PORT=${FRONTEND_PORT:-${FRONTEND_HOST##*:}}
-
-export API_DOMAIN=${API_DOMAIN:-${API_HOST%:*}}
-export API_PORT=${API_PORT:-${API_HOST##*:}}
-
 # strip https:// or https:// from domain if necessary
-# FRONTEND_DOMAIN=${FRONTEND_DOMAIN##*://}
-API_DOMAIN=${API_DOMAIN##*://}
+export APP_HOST=${APP_DOMAIN##*://}
 
-# echo using frontend: ${FRONTEND_DOMAIN} with port: ${FRONTEND_PORT}
 echo using API: ${API_DOMAIN} with port: ${API_PORT}
+echo using APP: ${APP_DOMAIN} with host: ${APP_HOST}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
