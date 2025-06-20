160dc98384e3f1bbeeecfed989448eab6bf2c68f
Zeeshan Ahmad <itexpert120@outlook.com>
2025-06-21 00:56:55 +0500

    fix: transparent toast bg for destructive variant

--- apps/app/src/components/ui/toast.tsx
+++ apps/app/src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
       variant: {
         default: "bg-white text-foreground", // Assuming 'foreground' is defined in Tailwind config
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-destructive bg-destructive text-destructive-foreground bg-red-100",
         success: "success group border-green-600 bg-green-100 text-green-800",
       },
     },
