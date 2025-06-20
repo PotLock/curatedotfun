0c9f6077a5418cebc0161e3c8f47f01829ebae42
Elliot Braem <elliot@ejlbraem.com>
2025-06-20 00:40:58 -0500

    server side

--- apps/app/src/lib/near.ts
+++ apps/app/src/lib/near.ts
@@ -1,6 +1,7 @@
+import { NETWORK_ID } from "@/config";
 import * as FastIntear from "fastintear";
 
 export const near: typeof FastIntear =
   typeof window !== "undefined"
     ? window.near // in browser
-    : FastIntear; // on server
+    : FastIntear && FastIntear.config({ networkId: NETWORK_ID }) && FastIntear; // on server
