5eb72a774fe4e95331bd3e1bba24f9ecf9c837c6
Elliot Braem <elliot@ejlbraem.com>
2025-06-25 12:36:42 -0500

    healing step, fix moderator

--- packages/shared-db/migrations/0020_purple_rictor.sql
+++ packages/shared-db/migrations/0020_purple_rictor.sql
@@ -1,4 +1,4 @@
-DROP INDEX "processing_steps_plugin_name_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "processing_steps_plugin_name_idx";--> statement-breakpoint
 ALTER TABLE "processing_steps" ADD COLUMN "plugin_name" text;--> statement-breakpoint
 ALTER TABLE "processing_steps" ADD COLUMN "config" jsonb;--> statement-breakpoint
 CREATE INDEX "processing_jobs_retry_check" ON "processing_jobs" USING btree ("id","retry_of_job_id") WHERE id != retry_of_job_id OR retry_of_job_id IS NULL;--> statement-breakpoint
--- packages/shared-db/src/schema/moderation.ts
+++ packages/shared-db/src/schema/moderation.ts
@@ -88,7 +88,6 @@ export const insertModerationHistorySchema = createInsertSchema(
 export const selectModerationHistorySchema = createSelectSchema(
   moderationHistory,
   {
-    id: z.coerce.number(),
     createdAt: z.date(),
     updatedAt: z.date().nullable(),
     moderatorAccountIdType: moderatorAccountIdTypeSchema,
